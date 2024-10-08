ce2746d99729ce8227effde8fd5c35d52d776c82
CelsodvJunior <celso_scjj@hotmail.com>
2024-10-08 00:13:10 -0300

    style: create file default theme

---
+++ src/utils/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  color: {
+    primary: "#5F1478",
+    secound: "#AE276F",
+    textCard: "#263238",
+    gradient: "linear-gradient(to right, #5F1478, #AE276F",
+  },
+
+  fonts: "Mulish, Helvetca, sans-serif",
+};
