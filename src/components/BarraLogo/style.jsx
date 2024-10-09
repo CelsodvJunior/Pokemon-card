50988adffeff8746b4329418258efab3d2663409
CelsodvJunior <celso_scjj@hotmail.com>
2024-10-09 11:21:01 -0300

    style: add responseviness to component BarraLogo

--- src/components/BarraLogo/style.jsx
+++ src/components/BarraLogo/style.jsx
@@ -3,13 +3,30 @@ import styled from "styled-components";
 export const Container = styled.div`
   background: ${({ theme }) => theme.color.gradient};
   width: 100%;
-  height: 50%;
+  height: 8vh;
   border: 1px, #00000029;
+  display: flex;
+  align-items: center;
+
+  @media (min-width: 767px) {
+    height: 10vh;
+  }
+
+  @media (min-width: 1023px) {
+    height: 8vh;
+  }
 `;
 
 export const LogImg = styled.img`
   background: transparent;
   height: 37px;
-  margin-left: 10px;
+  margin-left: 3%;
   padding: 5px;
+
+  @media (min-width: 1339px) {
+    height: 5vh;
+  }
+  @media (min-width: 1023px) {
+    height: 5vh;
+  }
 `;
