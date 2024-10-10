4e7c06280f3c9f2f10845625471c83441524e86c
CelsodvJunior <celso_scjj@hotmail.com>
2024-10-10 19:24:21 -0300

    feat: finalization of the home layout and giving functionality to the searchFild

--- src/pages/Home.jsx/index.jsx
+++ src/pages/Home.jsx/index.jsx
@@ -1,23 +1,51 @@
+import { useEffect, useState } from "react";
 import BarraLogo from "../../components/BarraLogo";
 import Button from "../../components/Button";
 import CardPokemon from "../../components/CardPokemon";
 import SearchField from "../../components/SearchField";
-import { Container, WrapperNewCard, P, WrapperCard } from "./style";
+import loadPokemon from "../../utils/loadPokemon";
+import { Container, P, WrapperCard, WrapperNewCard } from "./style";
 
 const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [allPokemons, setAllPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const pokemonData = await loadPokemon();
+        setPokemons(pokemonData);
+        setAllPokemons(pokemonData);
+      } catch (error) {
+        console.error("Failed to fetch Pokémons data:", error);
+      }
+    };
+    fetchPokemons();
+  }, []);
+
+  const PokemonFilter = (name) => {
+    if (name === "") {
+      setPokemons(allPokemons);
+    } else {
+      const filteredPokemons = allPokemons.filter((pokemon) => {
+        return pokemon.name.toLowerCase().includes(name.toLowerCase());
+      });
+      setPokemons(filteredPokemons);
+    }
+  };
+
   return (
     <Container>
       <BarraLogo />
-      <SearchField />
+      <SearchField PokemonFilter={PokemonFilter} />
       <WrapperNewCard>
         <P>Search result...</P>
         <Button>New Card</Button>
       </WrapperNewCard>
       <WrapperCard>
-        <CardPokemon />
-        <CardPokemon />
-        <CardPokemon />
-        <CardPokemon />
+        {pokemons.map((pokemon) => {
+          return <CardPokemon key={pokemon.id} pokemon={pokemon} />;
+        })}
       </WrapperCard>
     </Container>
   );
